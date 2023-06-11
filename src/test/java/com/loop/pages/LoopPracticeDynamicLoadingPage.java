c74162ea88540cf4795d1d5e860e8eae7fb923c2
Vafaismayil <vafaismayilzada@gmail.com>
2023-06-11 15:37:59 -0400

    commit

--- src/test/java/com/loop/pages/LoopPracticeDynamicLoadingPage.java
+++ src/test/java/com/loop/pages/LoopPracticeDynamicLoadingPage.java
@@ -1,32 +1,28 @@
 package com.loop.pages;
 
+
 import com.loop.test.utilities.BrowserUtils;
 import com.loop.test.utilities.ConfigurationReader;
 import com.loop.test.utilities.Driver;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
 public class LoopPracticeDynamicLoadingPage {
 
-    /**
-     *
 
-     @param nameOfthePage from loop practice page
-     once you on the page call it
-     @nsh*/
-    public static void loopLinkClick(String nameOfthePage){
-        WebElement element = Driver.getDriver().findElement(By.xpath("//a[.='" + nameOfthePage + "']"));
-        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.elementToBeClickable(element));
-        element.click();}
+    public LoopPracticeDynamicLoadingPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 
 
+    @FindBy(xpath = "//div[@id='alert']")
+    public WebElement doneMessage;
+
+    @FindBy(xpath = "//img[@src='/img/a-few-moments.jpg']")
+    public WebElement image;
     LoopPracticeDynamicLoadingPage page;
 
     @BeforeMethod
@@ -34,10 +30,36 @@ public class LoopPracticeDynamicLoadingPage {
         page = new LoopPracticeDynamicLoadingPage();
         Driver.getDriver().get(ConfigurationReader.getProperty("loop.practice"));
     }
+    @Test
+    public void test_method(){
+        BrowserUtils.loopLinkClick("Context Menu");
+    }
+
+
+
+
+}
+
+
+
+//    public static void loopLinkClick(String nameOfthePage){
+//        WebElement element = Driver.getDriver().findElement(By.xpath("//a[.='" + nameOfthePage + "']"));
+//        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+//        wait.until(ExpectedConditions.elementToBeClickable(element));
+//        element.click();}
+//
+//
+//    LoopPracticeDynamicLoadingPage page;
+//
+//    @BeforeMethod
+//    public void setUp(){
+//        page = new LoopPracticeDynamicLoadingPage();
+//        Driver.getDriver().get(ConfigurationReader.getProperty("loop.practice"));
+//    }
 
 //    @Test
 //    public void test_method(){
 //        BrowserUtils .loopLinkClick("Context Menu");
 //    }
 
-}
+
---
+++ src/test/java/com/loop/test/day9_driver_faker_action/T4_explicit_wait.java
@@ -0,0 +1,6 @@
+package com.loop.test.day9_driver_faker_action;
+
+public class T4_explicit_wait {
+
+
+}
